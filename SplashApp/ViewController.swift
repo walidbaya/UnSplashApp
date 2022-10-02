152e6f542b1a52350b20df813e5c3fd4fb516195
walidbaya <bayawid@hotmail.com>
2022-10-03 00:13:05 +0200

    Initial Commit

---
+++ SplashApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SplashApp
+//
+//  Created by Baya Walid on 03/10/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
